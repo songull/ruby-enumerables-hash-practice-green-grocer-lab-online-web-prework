ae66ff13b521ed738c1a948281d3e83aa8a89f6c
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-22 22:44:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,17 @@
 def consolidate_cart(cart)
   [
-  {"AVOCADO" => {:price => 3.00, :clearance => true }},
-  {"AVOCADO" => {:price => 3.00, :clearance => true }},
-  {"KALE"    => {:price => 3.00, :clearance => false}}
-]
+    {"AVOCADO" => {:price => 3.00, :clearance => true }},
+    {"AVOCADO" => {:price => 3.00, :clearance => true }},
+    {"KALE"    => {:price => 3.00, :clearance => false}}
+  ]
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  {
+  "AVOCADO" => {:price => 3.00, :clearance => true, :count => 3},
+  "KALE"    => {:price => 3.00, :clearance => false, :count => 1}
+}
+{:item => "AVOCADO", :num => 2, :cost => 5.00}
 end
 
 def apply_clearance(cart)
