9c4a5b5fc72dc8086a217416488de77bcf67b6f7
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-22 23:36:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,15 @@
 def consolidate_cart(cart)
-  [
-    {"AVOCADO" => {:price => 3.00, :clearance => true }}
-  ]
+  organized_cart = {}
+   count = 0
+   cart.each do |element|
+     element.each do |fruit, hash|
+       organized_cart[fruit] ||= hash
+       organized_cart[fruit][:count] ||= 0
+       organized_cart[fruit][:count] += 1
+     end
+   end
+   return organized_cart
+ end
 end
 
 def apply_coupons(cart, coupons)
